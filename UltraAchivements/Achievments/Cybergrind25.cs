1505ced69f185d22aa5f965c6d1ea3ed3b8537f8
Yuvraj <70811078+Protract-123@users.noreply.github.com>
2022-10-27 12:58:27 +0530

    Updated Cybergrind Achievement

--- UltraAchivements/Achievments/Cybergrind25.cs
+++ UltraAchivements/Achievments/Cybergrind25.cs
@@ -1,24 +1,45 @@
-﻿using HarmonyLib;
+﻿using GameConsole;
+using HarmonyLib;
 using System.IO;
 using UltraAchievement;
+using UnityEngine;
 
 namespace Ultrakill_Achivements.UltraAchivements.Achievments
 {
+    [HarmonyPatch(typeof(EndlessGrid), "Update")]
     public static class Cybergrind25
     {
-        [HarmonyPatch(typeof(EndlessGrid), "NextWave")]
-
-        public static void Postfix(EndlessGrid __instance)
+        public static void Postfix()
         {
-            string icon = $"{Directory.GetCurrentDirectory()}\\BepInEx\\plugins\\Sprites\\swordmachine.png";
-            string name = "Cybergrind Wave 25";
-            string description = "You reached cybergrind wave 25!";
-            string sprite = $"{Directory.GetCurrentDirectory()}\\BepInEx\\plugins\\Sprites\\modthing.jpeg";
-            string mod = "UltraAchievements Protract";
-            int i = __instance.currentWave;
-            if (i == 25)
+            
+            GameObject[] root = null;
+            if(root == null)
             {
-                Core.ShowAchievementI(icon, name, description, sprite, mod);
+                UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+            }
+            EndlessGrid arena = null;
+            if (root != null && arena == null)
+            {
+                foreach (GameObject obj in root)
+                {
+                    if (obj.name == "Everything")
+                    {
+                        arena = obj.GetComponentInChildren<EndlessGrid>();
+                    }
+                }
+            }
+            if (arena != null)
+            {
+                int i = arena.currentWave;
+                if (i == 25)
+                {
+                    string icon = $"{Directory.GetCurrentDirectory()}\\BepInEx\\plugins\\Sprites\\swordmachine.png";
+                    string name = "Cybergrind Wave 25";
+                    string description = "You reached cybergrind wave 25!";
+                    string sprite = $"{Directory.GetCurrentDirectory()}\\BepInEx\\plugins\\Sprites\\modthing.jpeg";
+                    string mod = "UltraAchievements Protract";
+                    Core.ShowAchievementI(icon, name, description, sprite, mod);
+                }
             }
         }
     }
